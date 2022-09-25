d93d371840fac0a3fa31c01664dd051ba464dc2d
Nathan viana <Nathancosta.muniz@outlook.com>
2022-09-25 17:33:55 -0300

    deu erro no codigo e tenho que perguntar pro luiz amanha

--- PucNowapp/models.py
+++ PucNowapp/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Student(models.Model):
+    username = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    course = models.CharField(max_length=30)
+    semester = models.CharField(max_length=30)
+    email = models.EmailField()
+    
 
+    
