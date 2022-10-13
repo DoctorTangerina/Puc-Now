95df5e67ae53abaf44a68c7081d44c061a16f5f0
NathanMuniz18 <nathancosta.muniz@outlook.com>
2022-10-13 10:48:42 -0300

    resquests finalizadas(views)

--- PucNowapp/views.py
+++ PucNowapp/views.py
@@ -4,5 +4,26 @@ from django.http import HttpResponse,request
 def PaginaInicial(request):
     return render(request,'login.html')
 
+def EsqueceuSenha(request):
+    return render(request,'esqueci.html')
+
+def Cadastro(request):
+    return render(request,'Cadastro.html')
+
+def Perfil(request):
+    return render(request,'Perfil.html')
+
+def Home(request):
+    return render(request,'home.html')
+
+def CriaGrupo(request):
+    return render(request,'CriaGrupo.html')
+
+def PesquisaGrupo(request):
+    return render(request,'PesquisaGrupo.html')
+
+def GrupoEstudos(request):
+    return render(request,'GrupoEstudos.html')
+
 
 
