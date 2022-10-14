15c3e2c9c1fcc4ea465607277a51230344859035
Doctor <arthursardellajiujitsu@gmail.com>
2022-10-13 21:38:20 -0300

    Banco de dados completo

--- PucNowapp/models.py
+++ PucNowapp/models.py
@@ -10,7 +10,30 @@ class Student(models.Model):
     semester = models.CharField(max_length=30)
     email = models.EmailField(max_length=40)
     password = models.CharField(max_length=40)
-    
-    
 
+class StudyGroup(models.Model):
+    groupId = models.PositiveIntegerField()
+    private = models.BooleanField()
+
+class Event(models.Model):
+    groupId = models.PositiveIntegerField()
+    local = models.CharField(max_length=20)
+    time = models.TimeField()
+    weekday = models.CharField(max_length=20)
+
+class PrivateChat(models.Model):
+    chatId = models.PositiveIntegerField()
+    userId1 = models.PositiveIntegerField()
+    userId2 = models.PositiveIntegerField()
+
+class GroupChat(models.Model):
+    chatId = models.PositiveIntegerField()
+    groupId = models.PositiveIntegerField()
     
+class Message(models.Model):
+    chatId = models.PositiveIntegerField()
+    groupId = models.PositiveIntegerField()
+    content = models.CharField(max_length=32767)
+    owner = models.CharField(max_length=20)
+    dateTimeSent = models.DateTimeField()
+    sent = models.BooleanField()
